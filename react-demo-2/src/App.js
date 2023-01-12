ba764cabd73e5767d70d7f597323c974a7d92738
karena chetan <chetan.karena24@exhibytesolution.com>
2023-01-12 11:16:39 +0530

    about componant

---
+++ react-demo-2/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+import About from "./Componant/About";
+function App() {
+  return (
+    <div className="App">
+      <About></About>
+    </div>
+  );
+}
+
+export default App;
